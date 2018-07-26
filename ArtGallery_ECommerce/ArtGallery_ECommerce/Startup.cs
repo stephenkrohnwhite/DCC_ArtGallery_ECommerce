a4656683b3b989f2021da20532fc0c7f07c0608e
Steve White <stephenkrohnwhite@gmail.com>
2018-07-26 18:55:32 -0500

    added createRole method in startup

--- ArtGallery_ECommerce/ArtGallery_ECommerce/Startup.cs
+++ ArtGallery_ECommerce/ArtGallery_ECommerce/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Owin;
+﻿using ArtGallery_ECommerce.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(ArtGallery_ECommerce.Startup))]
@@ -9,6 +12,24 @@ namespace ArtGallery_ECommerce
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRoles();
+        }
+        private void createRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+            }
         }
     }
 }
