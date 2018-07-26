28e0be0744d66b492cd40815e9d1d5e8f900e24e
Steve White <stephenkrohnwhite@gmail.com>
2018-07-26 18:29:31 -0500

    Adding solution with basic setup and bootswatch theme

---
+++ ArtGallery_ECommerce/ArtGallery_ECommerce/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArtGallery_ECommerce.Startup))]
+namespace ArtGallery_ECommerce
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
