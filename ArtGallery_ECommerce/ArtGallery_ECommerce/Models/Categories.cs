c4f3af9a79ccd63a29004b32a3a8adc90c9e4c19
Anina Reinhart <reinharta@outlook.com>
2018-07-26 20:07:01 -0500

    Initial Commit V.2

---
+++ ArtGallery_ECommerce/ArtGallery_ECommerce/Models/Categories.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ArtGallery_ECommerce.Models
+{
+    public class Categories
+    {
+        [Key]
+        int CategoryId { get; set; }
+        string Name { get; set; }
+    }
+}
---
+++ ArtGallery_ECommerce/ArtGallery_ECommerce/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ArtGallery_ECommerce.Models
+{
+    public class Employee
+    {
+        [Key]
+        int EmployeeID { get; set; }
+        string UserID { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+}
